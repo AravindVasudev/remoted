dfae2235b2f56b2782d8f86f2fbc808559b385dc
Aravind Vasudev <aravindv96@yahoo.com>
2017-08-21 06:52:51 +0530

    Rewrote to perform HTTP response

--- remoted.c
+++ remoted.c
@@ -18,6 +18,7 @@ int main(int argc, char const *argv[]) {
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
+    char *response = "HTTP/1.1 200 OK\r\nContent-Length:14\r\n\r\n<h1>Done.</h1>\r\n";
 
     Display *display;
     KeyCode alt_keycode, f4_keycode;
@@ -52,27 +53,38 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Accept Client
-    if ( (new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0 ) {
-        perror("Accept");
-        exit(EXIT_FAILURE);
+    while (1) {
+        // Accept Client
+        if ( (new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0 ) {
+            perror("Accept");
+            exit(EXIT_FAILURE);
+        }
+
+        valread = read(new_socket, buffer, 1024);
+        printf("%s\n", buffer);
+
+        if ( strstr(buffer, "/close") ) {
+            // Press Alt + F4 in X server
+            alt_keycode = XKeysymToKeycode(display, XK_Alt_L);
+            f4_keycode = XKeysymToKeycode(display, XK_F4);
+
+            XTestFakeKeyEvent(display, alt_keycode, True, 0);
+            XTestFakeKeyEvent(display, f4_keycode, True, 0);
+
+            XTestFakeKeyEvent(display, f4_keycode, False, 0);
+            XTestFakeKeyEvent(display, alt_keycode, False, 0);
+
+            XFlush(display);
+        } else if ( strstr(buffer, "/shutdown") ) {
+            // Shutdown
+            FILE *fp = popen("shutdown -r now", "r");
+            pclose(fp);
+        }
+
+        // respond to client
+        send(new_socket, response, strlen(response), 0);
+        printf("Response Sent!\n");
     }
-
-    // Shutdown
-    // FILE *fp = popen("shutdown -r now", "r");
-    // pclose(fp);
-
-    // Press Alt + F4 in X server
-    alt_keycode = XKeysymToKeycode(display, XK_Alt_L);
-    f4_keycode = XKeysymToKeycode(display, XK_F4);
-
-    XTestFakeKeyEvent(display, alt_keycode, True, 0);
-    XTestFakeKeyEvent(display, f4_keycode, True, 0);
-
-    XTestFakeKeyEvent(display, f4_keycode, False, 0);
-    XTestFakeKeyEvent(display, alt_keycode, False, 0);
-
-    XFlush(display);
 
     return 0;
 }
