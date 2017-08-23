a2ec99e134788e0caade303233c634ef0ac4f331
Aravind Vasudev <aravindv96@yahoo.com>
2017-08-23 07:29:41 +0530

    Added Open URL feature to GNU/Linux C variant

--- C/GNU_Linux/remoted.c
+++ C/GNU_Linux/remoted.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <regex.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,11 +18,22 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
+
     char buffer[1024] = {0};
     char *response = "HTTP/1.1 200 OK\r\nContent-Length:14\r\n\r\n<h1>Done.</h1>\r\n";
 
+    regex_t url;
+    regmatch_t match;
+    char extracted_url[512], open_command[512];
+
     Display *display;
     KeyCode alt_keycode, f4_keycode;
+
+    // Compile Regex
+    if ( regcomp(&url, "?url=", 0) ) {
+        perror("Cannot compile regular expression");
+        exit(EXIT_FAILURE);
+    }    
 
     // Open X Display
     display = XOpenDisplay(NULL);
@@ -63,7 +75,10 @@ int main(int argc, char const *argv[]) {
         valread = read(new_socket, buffer, 1024);
         printf("%s\n", buffer);
 
-        if ( strstr(buffer, "/close") ) {
+        if ( strstr(buffer, "/favicon.ico") ) {
+            // Ignore favicon requests
+            continue;
+        } else if ( strstr(buffer, "/close") ) {
             // Press Alt + F4 in X server
             alt_keycode = XKeysymToKeycode(display, XK_Alt_L);
             f4_keycode = XKeysymToKeycode(display, XK_F4);
@@ -79,6 +94,16 @@ int main(int argc, char const *argv[]) {
             // Shutdown
             FILE *fp = popen("shutdown -r now", "r");
             pclose(fp);
+        } else if ( strstr(buffer, "/open") ) {
+            // Extract the URL from the Header and open it
+            if ( !regexec(&url, buffer, 1, &match, 0) ) {
+                sprintf(extracted_url, "%*s", match.rm_eo - match.rm_so, buffer + match.rm_so + 5);
+            } else {
+                strcpy(extracted_url, "https://google.com");
+            }
+
+            snprintf(open_command, sizeof(open_command), "xdg-open %s", strtok(extracted_url, " "));
+            system(open_command);
         }
 
         // respond to client
